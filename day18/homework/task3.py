ddbbbfbaaddcbb357d5fba29e2a960925ffd44e2
Luks848 <khachidzeluka751@gmail.com>
2025-04-28 17:53:16 +0200

    commit

---
+++ day18/homework/task3.py
@@ -0,0 +1,7 @@
+"""4) მომხმარებელს შემოატანინეთ სახელი და თუ ის თქვენს სახელს ემთხვევა დაბეჭდეთ "coincidence"
+"""
+name= input("name: ")
+if name=="luka":
+    print("coincidence")
+else:
+    print("incorrect")
