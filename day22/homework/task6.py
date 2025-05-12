79095e2d3820cc5628e89c9c7ceaa176cc292580
Luks848 <khachidzeluka751@gmail.com>
2025-05-12 20:37:11 +0200

    commit

---
+++ day22/homework/task6.py
@@ -0,0 +1,11 @@
+"""7) შექმენით სია ციური სხეულებზე სიტყვებით და შემდეგ გამოიტანეთ მხოლოდ ის ელემენტები,
+რომლებიც ლუწ ინდექსებზე დგანან
+"""
+
+planets=["Mercury" , "sun" , "moon", "earth", "mars", "jupiter", "saturn", "neptune", "Uranus" "stars" ]
+print(len(planets))
+print(planets[0: 10: 2])
+
+
+
+
