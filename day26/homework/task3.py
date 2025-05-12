67ea762df6d3bfc458b13a0e91e52eb0b507339e
Luks848 <khachidzeluka751@gmail.com>
2025-05-12 22:07:10 +0200

    commit

---
+++ day26/homework/task3.py
@@ -0,0 +1,18 @@
+"""4) ახსენით კომენტარებით როგორ მუშაობს: lower, upper, capitalize მეთოდები
+
+"""
+
+# lower() მეთოდი:
+# გადაჰყავს სტრიქონი პატარა ასოებზე
+text = "Hello World"
+print(text.lower())  # შედეგი იქნება: "hello world"
+
+# upper() მეთოდი:
+# გადაჰყავს სტრიქონი დიდი ასოებზე
+text = "Hello World"
+print(text.upper()) 
+
+# capitalize() მეთოდი:
+# მხოლოდ პირველი ასო გადაჰყავს დიდ ასოზე, ხოლო დანარჩენები პატარა ასოებზე
+text = "hello world"
+print(text.capitalize())
